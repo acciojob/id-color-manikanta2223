dd9a4b97d4139aa5c18158ecb6362234cf4b6b6a
Manikanta <125333780+manikanta2223@users.noreply.github.com>
2023-09-24 13:09:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //your JS code here. If required.
-
+#yellow{
+	border-radius:1px;
+	border-color:green;
+}
+#red{
+	border-radius:1px;
+	border-color:black;
+}
+#yellow{
+	border-radius:1px;
+	border-color:yellow;
+}
